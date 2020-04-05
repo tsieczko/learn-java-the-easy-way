58c024b5977c01262cb5f3df7cb65d4e2ffeae78
Tomasz <sieczko.tomasz@gmail.com>
2020-04-04 21:56:15 -0700

    Finished chapter 2

---
+++ HiLo/src/HiLo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String playAgain = "";
+		int randomNumber = (int) (Math.random() * 100 + 1);
+		int guess = 0;
+
+		do {
+			while (guess != randomNumber) {
+				System.out.print("Guess a number between 1 and 100: ");
+				guess = scanner.nextInt();
+
+				if (guess < randomNumber) {
+					System.out.println(guess + " is too low. Try again.");
+				}
+				if (guess > randomNumber) {
+					System.out.println(guess + " is too high. Try again.");
+				}
+				if (guess == randomNumber) {
+					System.out.println(guess + " is correct. You win!");
+				}
+			}
+
+			System.out.println("Would you like to play again (y/n)?");
+			playAgain = scanner.next();
+		} while (playAgain.equalsIgnoreCase("y"));
+
+		System.out.println("Thank you for playing! Goodbye.");
+		scanner.close();
+	}
+
+}
